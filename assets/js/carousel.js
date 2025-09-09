f672ffdeb673bc84a2ce680dadf88366e261fb57
mauricio087 <m.river.cea@gmail.com>
2025-09-09 17:57:24 -0300

    carrusel: botones de cards sin uso

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -191,10 +191,6 @@ class ARCISCarousel {
                                             <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/>
                                         </svg>
                                     </button>
-                                    
-                                    <button class="btn btn-outline service-modal" data-modal="modal-${slide.ctaAction}">
-                                        Más Información
-                                    </button>
                                 </div>
                             </div>
                         </div>
